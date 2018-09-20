11f029d64ef349cf5083b0e63a5c98db4e449c39
octopustail <494479914@qq.com>
2018-09-20 16:49:51 +0800

    优化 做了开发环境和生产环境的区分

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,37 +1,5 @@
-import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import { routerReducer } from '../../node_modules/react-router-redux/lib/reducer'
-import { routerMiddleware } from 'react-router-redux'
-import { browserHistory } from 'react-router'
-
-import ThunkMiddleWare from 'redux-thunk'
-import rootReducer from './reducers'
-import createFetchMiddleware from 'redux-composable-fetch';
-import DevTools from './DevTools';
-
-const FetchMiddleware = createFetchMiddleware({
-    afterFetch({ action, result }) {
-        return result.json().then(data => {
-            return Promise.resolve({
-                action,
-                result: data,
-            });
-        });
-    },
-});
-
-
-const finalCreateStore = compose(
-    applyMiddleware(
-        ThunkMiddleWare,
-        FetchMiddleware,
-        routerMiddleware(browserHistory)),
-    DevTools.instrument()
-)(createStore)
-
-const reducers = combineReducers(Object.assign({}, rootReducer, {routing: routerReducer}));
-
-export default function configureStore(initialState) {
-    const store = finalCreateStore(reducers, initialState);
-    return store
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
-
---
+++ src/redux/configureStore.prod.js
@@ -0,0 +1,36 @@
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import { routerReducer } from '../../node_modules/react-router-redux/lib/reducer'
+import { routerMiddleware } from 'react-router-redux'
+import { browserHistory } from 'react-router'
+
+import ThunkMiddleWare from 'redux-thunk'
+import rootReducer from './reducers'
+import createFetchMiddleware from 'redux-composable-fetch';
+
+const FetchMiddleware = createFetchMiddleware({
+    afterFetch({ action, result }) {
+        return result.json().then(data => {
+            return Promise.resolve({
+                action,
+                result: data,
+            });
+        });
+    },
+});
+
+
+const finalCreateStore = compose(
+    applyMiddleware(
+        ThunkMiddleWare,
+        FetchMiddleware,
+        routerMiddleware(browserHistory)),
+    DevTools.instrument()
+)(createStore)
+
+const reducers = combineReducers(Object.assign({}, rootReducer, {routing: routerReducer}));
+
+export default function configureStore(initialState) {
+    const store = finalCreateStore(reducers, initialState);
+    return store
+}
+
