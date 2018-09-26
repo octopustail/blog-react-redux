ed637ec7b1164292b73afdd7e43854fb63a2222b
octopustail <494479914@qq.com>
2018-09-26 11:21:50 +0800

    add package.json

--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
 
-class Detail extends Component {
+class Nav extends Component {
     render() {
         return (
             <nav>
@@ -11,4 +11,4 @@ class Detail extends Component {
     }
 }
 
-export default Detail;
+export default Nav;
